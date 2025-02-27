a7eb9af8ce1ae4946994cb92a00ab84141f64bd5
Lee Joo Won <first0627@hanmail.net>
2025-02-28 00:09:44 +0900

    sdfd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,19 @@ import { BsFillPencilFill } from 'react-icons/bs';
 
 export default function Navbar() {
   return (
-    <header className='flex-justify-bwtween'>
-      <Link to='/'>
-        <FiShoppingBag />
-        <h1>Shoppy</h1>
-      </Link>
-      <nav>
-        <Link to='/products'>Products</Link>
-        <Link to='/carts'>Carts</Link>
-        <Link to='/products/new'>
-          <BsFillPencilFill />
+      <header className='flex justify-between border-b border-gray-300 p-2'>
+        <Link to='/' className='flex items-center text-4xl text-brand'>
+          <FiShoppingBag />
+          <h1>Shoppy</h1>
         </Link>
-        <button>Login</button>
-      </nav>
-    </header>
+        <nav className='flex items-center gap-4 font-semibold'>
+          <Link to='/products'>Products</Link>
+          <Link to='/carts'>Carts</Link>
+          <Link to='/products/new' className='text-2xl'>
+            <BsFillPencilFill />
+          </Link>
+          <button>Login</button>
+        </nav>
+      </header>
   );
 }
